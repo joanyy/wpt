30b5662fee6435ac9119478256cb9829dde420c1
Ian Clelland <iclelland@chromium.org>
2021-02-01 07:25:31 -0800

    Add new tests for credentials in reporting
    Change-Id: Icb36647d02ad33e8a2f10901782ad9b92cd0f191

--- reporting/resources/report-helper.js
+++ reporting/resources/report-helper.js
@@ -13,10 +13,18 @@ async function pollReports(endpoint, id) {
   return reports;
 }
 
+async function pollCookies(endpoint, id) {
+  const res = await fetch(`${endpoint}?reportID=${id}&op=retrieve_cookies`, {cache: 'no-store'});
+  const dict = await res.json();
+  if (dict.reportCookies == 'None')
+    return {};
+  return dict.reportCookies;
+}
+
 function checkReportExists(reports, type, url) {
   for (const report of reports) {
     if (report.type !== type) continue;
-    if (report.body.sourceFile === url) return true;
+    if (report.body.documentURL == url || report.body.sourceFile === url) return true;
   }
   assert_unreached(`A report of ${type} from ${url} is not found.`);
 }
